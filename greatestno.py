f72143a3f6e7a82988270e3e6b17d28842eb14b6
Neha <neha@Prabhats-MacBook-Air.local>
2025-12-26 11:04:36 +0530

    Hi

--- greatestno.py
+++ greatestno.py
@@ -6,7 +6,7 @@ num3=int(input("num3: "))
 
 if( (num1>num2) and (num1>num3)):
     print("Number",num1," is greatest number.")
-elif(num2>num1 and num2>num3):
+elif((num2>num1) and (num2>num3)):
     print("Number ",num2," is the greatest number.")
 else:
     print("Number", num3, "is the greatest number.")

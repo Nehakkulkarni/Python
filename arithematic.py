8173c38c52c2326e3bae905e0df5fcc00c4c591b
Neha <neha@Prabhats-MacBook-Air.local>
2025-12-28 15:56:53 +0530

    Hi

---
+++ arithematic.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+print("avg",a+b/2)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)

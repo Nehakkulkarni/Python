fd1dfe9c6cc6b6ea359b8aa93ca3807faad2441c
Neha <neha@Prabhats-MacBook-Air.local>
2025-12-31 21:35:56 +0530

    Hello

---
+++ Grades.py
@@ -0,0 +1,9 @@
+marks=int(input("Enter your marks:"))
+if(marks>=90):
+    print("A grade")
+elif(marks>=80):
+    print("B grade")
+elif(marks>=70):
+    print("C grade")
+else:
+    print("D grade")

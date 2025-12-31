8f37c513499a6a53ea672aa8bfda4832d58fa35d
Neha <neha@Prabhats-MacBook-Air.local>
2025-12-31 21:36:58 +0530

    Hello

---
+++ books.py
@@ -0,0 +1,11 @@
+book=[]
+book1=input("Enter 1st book: ")
+book2=input("Enter 2nd book: ")
+book3=input("Enter 3rd book: ")
+book.append(book1)
+book.append(book2)
+book.append(book3)
+print(book)
+
+
+

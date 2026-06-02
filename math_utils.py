5d29502b60fc589945a7c385abc727d4d2cb7205
Nehakkulkarni <nehakirankulkarni@gmail.com>
2026-06-02 20:04:18 +0530

    python

---
+++ math_utils.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    return a+b 
+def sub(a,b):
+    return a-b 
+def multi(a,b):
+    return a*b 
+def div(a,b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a/b 
